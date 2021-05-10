1a95152e8c21f2e567fe14c46496199209562685
Sijin <sijin@Sijinui-MacBookPro.local>
2021-05-10 23:46:21 +0900

    Fix get_channel_size helper function (#6)
    - 채널 사이즈 절반으로 줄일 때 채널 사이즈가 float형으로 반환됨
    - int형으로 기재된 테스트 코드와 비교시 발생하는 형변환 문제를 수정

--- pggan/helper.py
+++ pggan/helper.py
@@ -2,15 +2,16 @@ def get_channel_size(max_channels, min_channels):
 
     assert max_channels % 2 == 0 and min_channels % 2 == 0
 
-    channel_list = [(max_channel, max_channel)] * 4
+    channel_list = [(max_channels, max_channels)] * 4
 
-    channel = max_channel
+    channel = max_channels
     while channel >= min_channels:
         if channel == min_channels:
             channel_list.append((channel, 3))
+            channel = 3
 
         else:
-            channel_list.append((channel, channel/2))
-            channel = channel/2
+            channel_list.append((channel, int(channel/2)))
+            channel = int(channel/2)
 
     return channel_list
