fbd06fd549c5cc9fd9bafb993fabfd77f9e48ea9
Youngbeom Choi <helloybz@gmail.com>
2021-05-24 02:54:00 +0900

    Implement discriminator (#2)
     - 논문에 소개된 구성과는 약간 달리, 다음과 같이 나누어 구성되었다.
        - from_rgb
        - conv_blocks
        - minibatch_stddev
        - initial_conv_block
        - fc
     - conv_blocks는 ModuleList이며, feedforward할 때는  conv_block_cursor와 같은 index까지만
       이용된다.
     - Discriminator의 채널 크기 등은 구현 편의 상 Hard coding 되어있고, 추후 helper를 도입할 예정.

---
+++ tests/test_model.py
@@ -0,0 +1,27 @@
+import unittest
+
+import torch
+
+from pggan.models import Discriminator
+from pggan.models import MinibatchStdDev
+
+
+class TestDiscriminator(unittest.TestCase):
+    def test_discriminator(self):
+        discriminator = Discriminator()
+        B, C, H, W = 1, 3, 4, 4
+        x = torch.rand([B, C, H, W])
+        result = discriminator(x)
+        self.assertEqual(result.size(0), B)
+        self.assertEqual(result.size(1), 1)
+
+    def test_minibatch_stddev_forward(self):
+        minibatch_stddev = MinibatchStdDev()
+
+        B, C, H, W = 3, 3, 30, 30
+        x = torch.rand(B, C, H, W)
+        result = minibatch_stddev(x)
+        self.assertEqual(result.size(0), B)
+        self.assertEqual(result.size(1), C+1)
+        self.assertEqual(result.size(2), H)
+        self.assertEqual(result.size(3), W)
