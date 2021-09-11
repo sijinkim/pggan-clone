d3926cb35aa6ba060378b58c0d463ad2217ccf72
Sijin <sijin@Sijinui-MacBookPro.local>
2021-09-11 20:19:31 +0900

    CelebA-HQ 데이터셋 클래스 작성(#8)
    - RGB 채널 사용
    - [0.0, 1.0] 스케일링 적용

--- pggan/dataset.py
+++ pggan/dataset.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import cv2 as cv
 import torch
 from torch.utils.data import Dataset
 
@@ -11,13 +12,18 @@ class CelebAHQ(Dataset):
         split:      str,
     ) -> None:
         super(CelebAHQ, self).__init__()
-        data_paths = data_root.joinpath(split)
+        data_paths = data_root.joinpath(split).glob('*.jpg')
+        self.im_list = [im for im in data_paths]
 
     def __getitem__(
         self,
         idx:    int
     ) -> torch.Tensor:
-        return idx
+        im_np = cv.imread(str(self.im_list[idx]))[
+            :, :, [2, 1, 0]]  # BGR to RGB
+        im_tensor = torch.FloatTensor(
+            im_np/255).permute(2, 0, 1)  # [0, 255] to [0, 1]
+        return im_tensor
 
     def __len__(self):
-        return 0
+        return len(self.im_list)
--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,9 +1,25 @@
-
 import unittest
 from pathlib import Path
+
+import torch
+
+from pggan.dataset import CelebAHQ
 
 
 class TestDataset(unittest.TestCase):
     def setUp(self):
         data_root = Path('./tests/data_root').absolute()
-        print(data_root)
+        self.dataset = CelebAHQ(data_root, 'train')
+
+    def test_is_tensor(self):
+        sample_data = self.dataset[0]
+        self.assertIsInstance(sample_data, torch.Tensor)
+
+    def test_data_shape(self):
+        sample_data = self.dataset[0]
+        self.assertEqual(sample_data.shape, torch.Size([3, 1024, 1024]))
+
+    def test_data_scale(self):
+        sample_data = self.dataset[0]
+        self.assertGreaterEqual(sample_data.min(), 0.0)
+        self.assertLessEqual(sample_data.max(), 1.0)
