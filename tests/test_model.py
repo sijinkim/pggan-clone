2c4e7e8eabfb1aea501b444c3346cad68af085f7
Youngbeom Choi <helloybz@gmail.com>
2021-11-18 12:09:29 +0900

    Fix Discriminator
     - 모델의 구성요소를 다음과 같이 변경함:
         - fromRGB
         - conv_blocks
         - minibatch_stddev
         - fc
     - Discriminator를 초기화할 때, input image resolution이 4x4임을 가정하고 모델을 구성함.
     - set_input_image_size 메소드를 통해 input image resolution에 맞게 모델 구조를 변경함.
     - set_input_image_size 메소드가 변경하는 부분은:
         - 새로운 fromRGB 모듈로 변경
         - 새로운 conv_block을 구성하여 self.conv_blocks 맨앞에 추가

--- tests/test_model.py
+++ tests/test_model.py
@@ -7,14 +7,6 @@ from pggan.models.modules import MinibatchStdDev
 
 
 class TestDiscriminator(unittest.TestCase):
-    def test_discriminator(self):
-        discriminator = Discriminator()
-        B, C, H, W = 1, 3, 4, 4
-        x = torch.rand([B, C, H, W])
-        result = discriminator(x)
-        self.assertEqual(result.size(0), B)
-        self.assertEqual(result.size(1), 1)
-
     def test_minibatch_stddev_forward(self):
         minibatch_stddev = MinibatchStdDev()
 
@@ -25,3 +17,18 @@ class TestDiscriminator(unittest.TestCase):
         self.assertEqual(result.size(1), C+1)
         self.assertEqual(result.size(2), H)
         self.assertEqual(result.size(3), W)
+
+    def test_discriminator(self):
+        discriminator = Discriminator()
+        input_image_sizes = [4, 8, 16, 32, 64, 128, 256, 512, 1024]
+
+        for input_image_size in input_image_sizes:
+
+            B, C, H, W = 2, 3, input_image_size, input_image_size
+            x = torch.rand(B, C, H, W)
+
+            discriminator.set_input_image_size(input_image_size)
+            result = discriminator(x)
+            self.assertEqual(result.dim(), 2)
+            self.assertEqual(result.size(0), B)
+            self.assertEqual(result.size(1), 1)
