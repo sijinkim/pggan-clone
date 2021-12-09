af67474e8312e98f2885a9e4e9d1e2ac7353bc8e
Sijin <sijin@Sijin-MacBookPro.local>
2021-12-09 21:04:07 +0900

    Create generator's get_out_channels test function
    - 인풋 이미지 사이즈를 기준으로 채널사이즈가 계산되는 것을 확인합니다.
    - Generator 클래스의_get_out_channels 함수의 동작을 확인합니다.

--- tests/test_modules.py
+++ tests/test_modules.py
@@ -9,14 +9,14 @@ from pggan.models.modules import MinibatchStdDev
 
 
 class TestGenerator(unittest.TestCase):
-    def setUp(self):
-        self.generator = Generator()
-        self.sample_input = torch.from_numpy(
-            np.random.randn(512, 1, 1)).float()
+    def test_get_out_channels(self):
+        resolutions = [1, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
+        ref_channels = [512, 512, 512, 512, 512, 256, 128, 64, 32, 16]
+        generator = Generator()
 
-    def test_output_shape(self):
-        self.assertEqual(torch.Size([1, 3, 1024, 1024]),
-                         self.generator(torch.unsqueeze(self.sample_input, 0)).shape)
+        for res, ref in zip(resolutions, ref_channels):
+            print(generator._get_out_channels(res), ref)
+            self.assertEqual(generator._get_out_channels(res), ref)
 
 
 class TestDiscriminator(unittest.TestCase):
@@ -49,7 +49,8 @@ class TestDiscriminator(unittest.TestCase):
 
             channels = [16, 32, 64, 128, 256, 512]
             self.assertSetEqual(
-                set([int(fromRGB_key.split('to_')[-1]) for fromRGB_key in discriminator.fromRGBs.keys()]),
+                set([int(fromRGB_key.split('to_')[-1])
+                    for fromRGB_key in discriminator.fromRGBs.keys()]),
                 set(channels)
             )
 
